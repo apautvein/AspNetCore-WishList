66c06625c4104c63453a86b0743b9375f73d4170
apautvein <apautvein@gmail.com>
2020-10-10 10:50:45 -0500

    Create home views and the homeController

---
+++ WishList/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WishList.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();  
+        }
+    }
+}
