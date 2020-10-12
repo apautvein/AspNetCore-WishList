9caa0b8689380ecfe016b4d746644fdeaabc41bc
apautvein <apautvein@gmail.com>
2020-10-12 12:51:44 -0500

    fixed files so pleuralsite tests pass

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using WishList.WishList.Models;
+using WishList.Models;
 
 namespace WishList.Data
 {
